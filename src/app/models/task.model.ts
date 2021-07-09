bbec819e27b4e327d464594c5d4dd123e0443f7c
xlpnsk <adam.lipinski@pollub.edu.pl>
2021-07-09 20:59:58 +0200

    Add Task model

---
+++ src/app/models/task.model.ts
@@ -0,0 +1,11 @@
+export interface Task{
+    id:number;
+    description:string
+    startDate:string
+    stopDate:string
+    staffId:number
+    typeId:number
+    startTime:string
+    stopTime:string
+    day:number
+}
