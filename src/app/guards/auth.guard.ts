e2d99eef75356e512f661bbf5829fbef14c21e8d
xlpnsk <adam.lipinski@pollub.edu.pl>
2021-07-25 14:53:27 +0200

    Create auth guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../api.service';
+import { filter, map, take } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  // canLoad(
+  //   route: Route,
+  //   segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
+
+  constructor(private api: ApiService, private router: Router) { }
+ 
+  canActivate(): Observable<boolean> {   
+    return this.api.currentUser.pipe(
+      filter(val => val !== null), // Filter out initial Behaviour subject value
+      take(1), // Otherwise the Observable doesn't complete!
+      map(isAuthenticated => {
+        if (isAuthenticated) {          
+          return true;
+        } else {          
+          this.router.navigateByUrl('/login')
+          return false;
+        }
+      })
+    );
+  }
+}
