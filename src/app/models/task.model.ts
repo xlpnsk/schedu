1c94be39afbad45b5be67c8ea49e49db3d7e579c
xlpnsk <adam.lipinski@pollub.edu.pl>
2021-08-14 18:20:11 +0200

    Update task model

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -9,3 +9,20 @@ export interface Task{
     stopTime:string
     day:number
 }
+
+export interface AdminTask{
+    id:number;
+    description:string
+    startDate:string
+    stopDate:string
+    Staff:{
+        email:string
+    }
+    TaskType:{
+        name:string
+    }
+    startTime:string
+    stopTime:string
+    day:number
+}
+
