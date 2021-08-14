e250df69bc355b0d2b6397e7032e14e0a9a00739
xlpnsk <adam.lipinski@pollub.edu.pl>
2021-08-14 18:29:23 +0200

    Add new routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { ContactComponent } from './contact/contact.component';
+import { NewStaffComponent } from './forms/new-staff/new-staff.component';
 import { NewTaskComponent } from './forms/new-task/new-task.component';
 import { PasswdComponent } from './forms/passwd/passwd.component';
+import { AuthAdminGuard } from './guards/auth-admin.guard';
 import { AuthGuard } from './guards/auth.guard';
 import { InfoComponent } from './info/info.component';
 import { MainComponent } from './main/main.component';
@@ -10,17 +14,22 @@ import { StaffComponent } from './staff/staff.component';
 import { AccountComponent } from './user/account/account.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { TaskListComponent } from './user/task-list/task-list.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent},
   { path: 'schedule', component: CalendarComponent},
   { path: 'info', component: InfoComponent},
   { path: 'staff', component: StaffComponent},
+  { path: 'contact', component: ContactComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'account', component: AccountComponent, canActivate: [AuthGuard]},
-  { path: 'task', component: NewTaskComponent, canActivate: [AuthGuard]},
-  { path: 'password', component: PasswdComponent, canActivate: [AuthGuard]}
+  { path: 'account/task', component: NewTaskComponent, canActivate: [AuthGuard]},
+  { path: 'password', component: PasswdComponent, canActivate: [AuthGuard]},
+  { path: 'account/tasks', component: TaskListComponent, canActivate: [AuthGuard]},
+  { path: 'admin', component: AdminPanelComponent, canActivate: [AuthAdminGuard,AuthGuard]},
+  { path: 'admin/staff', component: NewStaffComponent, canActivate: [AuthAdminGuard,AuthGuard]},
 ];
 
 @NgModule({
