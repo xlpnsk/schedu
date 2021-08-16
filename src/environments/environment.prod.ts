fd0c1e8cd181ea3cc0696063ba2b71fa7d3fb460
xlpnsk <adam.lipinski@pollub.edu.pl>
2021-08-16 17:42:13 +0200

    Add env values for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  SUPABASE_URL: 'https://yjuyvcstrnpwpqrcwbnb.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNDk3MzE4MCwiZXhwIjoxOTQwNTQ5MTgwfQ.2RwvbWzn_n8oJCKPpLDQ0b9cLlSirwpM15xaPASz4RQ'
 };
